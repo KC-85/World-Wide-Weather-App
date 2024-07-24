09c8952f842e16e6bea4fdacde1c125924e345f5
Kristian Cross <etamin1985@yahoo.com>
2024-07-24 22:29:22 +0000

    Modify JavaScript code

--- script.js
+++ script.js
@@ -42,6 +42,24 @@ async function weatherFn(cName) {
 }
 
 function weatherShowFn(data) {
+    const weatherCard = document.querySelector('.weather-card');
+    const weatherConditions = {
+        Clear: 'clear.jpg',
+        Clouds: 'clouds.jpg',
+        Rain: 'rain.jpg',
+        Drizzle: 'drizzle.jpg',
+        Thunderstorm: 'thunderstorm.jpg',
+        Snow: 'snow.jpg',
+        Mist: 'mist.jpg',
+    };
+
+    const condition = data.weather[0].main;
+    const backgroundImage = weatherConditions[condition] || 'default.jpg';
+
+    weatherCard.style.backgroundImage = `url('images/${backgroundImage}')`;
+    weatherCard.style.backgroundSize = 'cover';
+    weatherCard.style.backgroundPosition = 'center';
+
     $('#city-name').text(data.name);
     $('#date').text(moment().format('MMMM Do YYYY, hh:mm:ss a'));
     $('#temperature').html(`${data.main.temp}°C`);
